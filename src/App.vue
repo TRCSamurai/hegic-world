0a007da3b41b30ffa913a080b4a23cfa254940f3
CryptoSamurai <therealcryptosamurai@gmail.com>
2020-12-18 23:44:20 +0000

    aligment fixes

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
       </div>
       <div class="container mx-auto px-8 py-8 md:py-12">
         <div class="flex flex-wrap -mx-8">
-          <div class="w-full lg:w-1/2 px-8">
+          <div class="w-full lg:w-1/2 px-8 mb-8 lg:mb-0">
             <category-title>Documents</category-title>
             <div class="flex grid gap-8 grid-cols-1 sm:grid-cols-2">
               <document v-for="document in documents" :title="document.title" :url="document.url" :key="document.title"/>
@@ -32,9 +32,9 @@
     </main>
     <footer>
       <div class="container mx-auto px-8 pt-12">
-        <p class="text-center text-sm mb-2">Made by<a href="https://twitter.com/CryptoSamurai" target="_blank"
-                                                      class="ml-1 text-primary">🧙‍♂ CryptoSamurai</a>.
-          <a href="https://etherscan.io/address/0x11Ac41eF94D95197525620275e11a5Cb8c32363C" target="_blank"> Buy me a 🍪</a>. <a href="https://github.com/" class="underline" target="_blank"> Contribute</a></p>
+        <p class="text-center text-sm mb-2">Made by <a href="https://twitter.com/CryptoSamurai" target="_blank"
+                                                      class="ml-1 mr-1 text-primary">🧙‍♂CryptoSamurai</a>.
+          <a href="https://etherscan.io/address/0x11Ac41eF94D95197525620275e11a5Cb8c32363C" target="_blank"> Buy me a 🍪</a>. <a href="https://github.com/TRCSamurai/hegic-world" class="underline ml-1" target="_blank"> Contribute</a></p>
       </div>
     </footer>
   </div>
