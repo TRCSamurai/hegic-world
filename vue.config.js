5167c63e9f0a0a4ffc6933f3c1115d917ce988e1
CryptoSamurai <therealcryptosamurai@gmail.com>
2021-01-22 10:15:56 +0000

    add Hegic Visual Analytics

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,30 @@ module.exports = {
                 '@': path.resolve(__dirname, 'src/')
             },
         }
+    },
+    pluginOptions: {
+        s3Deploy: {
+            registry: undefined,
+            awsProfile: 'default',
+            overrideEndpoint: true,
+            endpoint: 's3.amazonaws.com',
+            region: 'us-west-2',
+            bucket: 'hegic-world',
+            createBucket: true,
+            staticHosting: true,
+            staticIndexPage: 'index.html',
+            staticErrorPage: 'index.html',
+            assetPath: 'dist',
+            assetMatch: '**',
+            deployPath: '/',
+            acl: 'public-read',
+            pwa: true,
+            pwaFiles: 'index.html,service-worker.js,manifest.json',
+            enableCloudfront: true,
+            cloudfrontId: 'EYJGJ8V7E8FHB',
+            cloudfrontMatchers: '/index.html,/service-worker.js,/manifest.json',
+            pluginVersion: '4.0.0-rc3',
+            uploadConcurrency: 5
+        }
     }
 }
